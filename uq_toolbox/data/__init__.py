f9b5d72e66567cf7bc3a6624fd1f87e9d07b04fa
Michelangelo Urbano <ertonnozz@gmail.com>
2026-08-19 01:19:51 +0200

    Delete uq_toolbox directory

--- uq_toolbox/data/__init__.py
+++
@@ -1,9 +0,0 @@
-# ==========================================
-# UQ TOOLBOX - DATA MODULE INITIALIZATION
-# ==========================================
-
-from .data_loader import prepare_dataset
-
-__all__ = [
-    "prepare_dataset",
-]
